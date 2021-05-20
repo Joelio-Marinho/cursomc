e1a57eaaf2a2b26b7c3afc07b04991fbe92be59c
Joelio Marinho <joeliomarinho@gmail.com>
2021-05-20 10:33:10 -0300

    banco de dados h2 e criação da base de dados

--- src/main/java/com/curso/cursomc/model/Categoria.java
+++ src/main/java/com/curso/cursomc/model/Categoria.java
@@ -1,11 +1,18 @@
 package com.curso.cursomc.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity(name= "CATEGORIA")
 public class Categoria  implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String nome;
